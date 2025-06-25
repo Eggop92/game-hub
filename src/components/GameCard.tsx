18de91a175d21b78d618b7c9230785aed5a12f96
epuerta <egoitzpuerta1992@gmail.com>
2025-06-25 10:57:04 +0200

    Remove incorrect interfaces/imports

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,5 @@
-import { Card, CardBody, Heading, HStack, Image, Text } from '@chakra-ui/react'
-import React from 'react'
-import { Game } from '../hooks/useGames'
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react'
+import { Game } from '../services/gameServices'
 import getCroppedImageUrl from '../services/image-url'
 import CriticScore from './CriticScore'
 import Emoji from './Emoji'
@@ -19,7 +18,7 @@ const GameCard = ({ game }: Props) => {
           <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top}/></Heading>
+        <Heading fontSize='2xl'>{game.name}<Emoji rating={game.rating_top} /></Heading>
       </CardBody>
     </Card>
   )
