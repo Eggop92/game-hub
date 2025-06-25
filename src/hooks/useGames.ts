05c8341f084c71faeef2b24d629bae13538e028f
epuerta <egoitzpuerta1992@gmail.com>
2025-06-25 10:51:13 +0200

    fetch games with useQuery

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,29 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import { Platform } from "../services/platformService";
-import useData from "./useData";
+import { FetchResponse } from "../services/api-client";
+import getGames, { Game } from "../services/gameServices";
 
 
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
+
+
+const useGames = (gameQuery: GameQuery) => {
+  return useQuery<FetchResponse<Game>, Error>({
+    queryKey: ['games', gameQuery],
+    queryFn: getGames.bind(gameQuery),
+    staleTime: 24 * 60 * 60 * 1000
+  });
 }
-
-const useGames = (gameQuery: GameQuery) =>
-  useData<Game>(
-    "/games",
-    {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
-    },
-    [gameQuery]
-  );
 
 export default useGames;
